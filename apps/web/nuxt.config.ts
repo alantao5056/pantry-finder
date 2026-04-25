b2a630ef1d7f628450f31513f124f51fdb215f7a
alantao5056 <alantaolr@outlook.com>
2026-04-25 18:09:53 -0400

    Configure deploy.yml for web app

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8080',
+      apiBase: process.env.REST_API_URL || 'http://localhost:8080',
     },
   },
   app: {
